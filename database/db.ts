65052f02af71c0f5fef03942341e9c0a7297dcf1
Elgrueber <elenagrueber@gmail.com>
2024-09-01 20:26:01 -0400

    change DB settings + 2 new tests

--- database/db.ts
+++ database/db.ts
@@ -1,31 +1,46 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { ConnectionPool } from 'mssql';
+import { ConnectionPool, config as SQLConfig } from 'mssql';
 
 interface DBConfig {
-  user: string;
-  host: string;
-  database: string;
-  password: string;
-  port: number;
+    user: string;
+    host: string;
+    database: string;
+    password: string;
+    port: number;
 }
 
 export class DB {
-  async executeQuery(query: string, config) {
-    const client = new ConnectionPool(config);
-    try {
-      await client.connect();
-      const result = await client.query(query);
-      console.log(result.rows);
-      return result.rows;
-    } catch (error) {
-      console.error("Connection/Query Execution Error:", error);
-      throw error;
-    } finally {
-      
-      }
+    constructor(private config: DBConfig) {}
+
+    private getPoolConfig(): SQLConfig {
+        return {
+            user: this.config.user,
+            server: this.config.host,
+            database: this.config.database,
+            password: this.config.password,
+            port: this.config.port,
+            options: {
+                encrypt: true,
+            },
+        };
     }
-  }
 
+    async executeQuery(query: string) {
+        const poolConfig = this.getPoolConfig();
+        const client = new ConnectionPool(poolConfig);
 
+        try {
+            await client.connect();
+            const result = await client.request().query(query);
+            console.log(result.recordset);
+            return result.recordset;
+        } catch (error) {
+            console.error('Connection/Query Execution Error:', error);
+            throw error;
+        } finally {
+            await client.close();
+        }
+    }
+}
---
+++ tests/database.spec.ts
@@ -0,0 +1,71 @@
+import { test, expect } from '@playwright/test';
+import { DB } from '../database/db';
+import { EMAILUSER, PASSWORDUSER } from '../testData';
+
+const dbConfig = {
+    user: process.env.DB_USER!,
+    host: process.env.DB_HOST!,
+    database: process.env.DB_NAME!,
+    password: process.env.DB_PASSWORD!,
+    port: parseInt(process.env.DB_PORT!, 10),
+};
+
+const database = new DB(dbConfig);
+
+test.describe('Database testing', () => {
+    test('Verify there are users in DB', async () => {
+        const result = await database.executeQuery('SELECT COUNT(*) AS userCount FROM [user];');
+
+        const userCount = result[0]?.userCount;
+        expect(userCount).toBeGreaterThan(0);
+        console.log('Number of users in DB: ' + userCount);
+    });
+
+    test('Verify successfully added new user', async ({ page }) => {
+        const userEmail = EMAILUSER;
+        const userPassword = PASSWORDUSER;
+        let query = `
+    INSERT INTO [user] (email, password)
+    VALUES ('${userEmail}', '${userPassword}');
+  `;
+
+        try {
+            const result = await database.executeQuery(query);
+            console.log('New user is added successfully');
+        } catch (error) {
+            console.error('Error while adding new user:', error);
+        }
+
+        query = `select * from [user] where email = '${userEmail}';`;
+        const result = await database.executeQuery(query);
+
+        expect(result.length).toBe(1);
+        expect(result[0]?.email).toBe(userEmail);
+    });
+
+    test('Verify successfully updated user email', async () => {
+        const userEmail = EMAILUSER.substring(1);
+        const userPassword = PASSWORDUSER;
+        const newEmail = userEmail.substring(2);
+        console.log(newEmail);
+
+        let query = `
+      INSERT INTO [user] (email, password)
+      VALUES ('${userEmail}', '${userPassword}');
+    `;
+        let result = await database.executeQuery(query);
+
+        const updateQuery = `
+        UPDATE [user]
+        SET email = '${newEmail}'
+        WHERE email = '${userEmail}';
+      `;
+        result = await database.executeQuery(updateQuery);
+
+        query = `select * from [user] where email = '${newEmail}';`;
+        result = await database.executeQuery(query);
+
+        expect(result.length).toBe(1);
+        expect(result[0]?.email).toBe(newEmail);
+    });
+});
