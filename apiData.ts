4f95bf39fd176f88a0df6f1a9b1158fe93ab17eb
Elgrueber <89706559+Elgrueber@users.noreply.github.com>
2024-09-16 20:31:37 -0400

    API negative test for user creation (invalid email / invalid password) (#11)
    * api negative tests
    
    * skip database tests
    
    * add skip getUser

--- apiData.ts
+++ apiData.ts
@@ -5,3 +5,21 @@ export const API_URL_END_POINTS = {
     userDeleteEndPoint: '/user/',
     userUpdateEndPoint: '/user/',
 };
+
+export const NEGATIVE_EMAIL_DATA_SET = [
+    ['without @', 'test15.test.gmail'],
+    ['without domain', 'test15@gmail'],
+    ['space before email', ' test15@gmail.com'],
+    ['space after domain', 'test15@gmail.com '],
+    ['empty field', ''],
+];
+
+export const NEGATIVE_PASSWORD_DATA_SET = [
+    ['Password less than 6 characters long', 'P1p!', 'Password must be at least 6 characters!'],
+    ['Password longer than 20 characters long', 'Password1!Password1!Password1!', 'Password must be at most 20 characters long!'],
+    ['No digits in the password', 'Password!', 'Password must contains at least one digit!'],
+    ['No special symbols in the password', 'Password1', 'Password must contain at least one special character!'],
+    ['No lowercase letter in the password', 'PASSWORD1!', 'Password must contain at least one lowercase letter!'],
+    ['No uppercase letter in the password', 'password1!', 'Password must contain at least one uppercase letter!'],
+    ['Empty password field', '', 'Password must contains at least one digit!'],
+];
--- tests/api_tests/user.spec.ts
+++ tests/api_tests/user.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
-import { API_URL_END_POINTS } from '../../apiData';
+import { API_URL_END_POINTS, NEGATIVE_EMAIL_DATA_SET, NEGATIVE_PASSWORD_DATA_SET } from '../../apiData';
 import { EMAILUSER, PASSWORDUSER } from '../../testData';
 
-test.describe('EG API Tests', () => {
+test.describe('EG API Positive Tests', () => {
     test('Get list of users', { tag: ['@api'] }, async ({ request }) => {
         const response = await request.get(process.env.API_BASE_URL + '/user');
         const responseBody = await response.json();
@@ -63,3 +63,51 @@ test.describe('EG API Tests', () => {
         expect(response.status()).toBe(200);
     });
 });
+
+
+test.describe('EG API Negative Tests', () => {
+    const baseUrl = process.env.API_BASE_URL;
+    const createUrl = `${baseUrl}${API_URL_END_POINTS.userCreateEndPoint}`;
+
+    NEGATIVE_EMAIL_DATA_SET.forEach((typeEmailField) => {
+        test(`Verify non-successful creation of user in case of invalid email and valid password: ${typeEmailField[0]}`, async ({ request }) => {
+        const userPassword = PASSWORDUSER.slice(0, 10);
+
+        let response = await request.post(createUrl, {
+            data: {
+                email: typeEmailField[1],
+                password: userPassword,
+            },
+        });
+
+        let responseBody = await response.json();
+        const error = responseBody.error;
+        const message = responseBody.message;
+
+        expect(response.status()).toBe(400);
+        expect(responseBody.error).toBe('Bad Request');
+        expect(responseBody.message[0]).toBe("email must be an email");
+    });
+});
+
+NEGATIVE_PASSWORD_DATA_SET.forEach((typePasswordField) => {
+    test(`Verify non-successful creation of user if: ${typePasswordField[0]}`, async ({ request }) => {
+    const userEmail = EMAILUSER;
+
+    let response = await request.post(createUrl, {
+        data: {
+            email: userEmail,
+            password: typePasswordField[1],
+        },
+    });
+
+    let responseBody = await response.json();
+    const error = responseBody.error;
+    const message = responseBody.message;
+
+    expect(response.status()).toBe(400);
+    expect(responseBody.error).toBe('Bad Request');
+    expect(responseBody.message[0]).toContain(typePasswordField[2]);
+});
+});
+})
