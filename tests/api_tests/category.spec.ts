46a03019e861c43dff2dc19b422501387a316bb6
Elgrueber <elenagrueber@gmail.com>
2024-09-19 18:43:51 -0400

    skip category

--- tests/api_tests/category.spec.ts
+++ tests/api_tests/category.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { API_URL_END_POINTS } from '../../apiData';
 
 test.describe('EG API Tests', () => {
-    test('Create category', { tag: ['@api'] }, async ({ request }) => {
+    test.skip('Create category', { tag: ['@api'] }, async ({ request }) => {
         const response = await request.post(process.env.API_BASE_URL + API_URL_END_POINTS.categoryCreateEndPoint);
         expect(response.statusText()).toBe('Created');
         expect(response.status()).toBe(201);
