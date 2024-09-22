953e00e1c93f32fc0f5b60a198b28fdd2b4b8a31
Elgrueber <elenagrueber@gmail.com>
2024-09-22 17:16:30 -0400

    fix package-lock

--- global-setup.ts
+++ global-setup.ts
@@ -4,11 +4,9 @@ import path from 'path';
 import fs from 'fs';
 
 module.exports = async () => {
-    // Determine the environment, default to 'dev'
     const env = process.env.ENVIRONMENT || 'dev';
     const envFilePath = path.resolve(__dirname, 'env', `${env}.env`);
 
-    // Load environment variables from the specified .env file
     if (fs.existsSync(envFilePath)) {
         dotenv.config({ path: envFilePath, override: true });
         console.log(`Environment variables loaded from ${env}.env file`);
