3bb30b09a50e08f6edc1fdf6da28d826f238694e
LinaAbu1306 <lina.qa.abu@gmail.com>
2024-09-23 22:35:02 -0400

    Retrieve all users by get api/user

--- tests/api_tests/getUser.spec.ts
+++ tests/api_tests/getUser.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect } from '@playwright/test';
+import { API_URL_END_POINTS } from '../../apiData';
 
 test.skip('should check response status', { tag: ['@api'] }, async ({ request }) => {
     const response = await request.get(process.env.API_BASE_URL + '/user');
     console.log(JSON.stringify(response));
     expect(response['_initializer'].status).toBe(200);
 });
-test('Verify 401 Unauthorized response status', async ({request}) =>{
-    const response = await request.get (process.env.API_BASE_URL + '/user')
-    expect (response.status()).toBe(401)
-    const responseMessage = await response.text()
-    expect(responseMessage).toContain('Unauthorized')
-})
+test('Retrieve all users by get api/user', async ({ request }) => {
+        const response = await request.get(process.env.API_BASE_URL + API_URL_END_POINTS.userEndPoint);
+        expect(response.status()).toBe(401);
+        
+        const responseMessage = await response.text();
+        expect(responseMessage).toContain('Unauthorized');
+    });
