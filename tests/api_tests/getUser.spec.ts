7ca99985e1fe1c351933f4c2e4bae319fdd4c776
LinaAbu1306 <Lina.qa.abu@gmail.com>
2024-09-28 22:59:14 -0400

    "Temporary test to assert 401 status" (#21)
    * "Temporary test to assert 401 status"
    
    * Verify 401 Unauthorized response status
    
    * Retrieve all users by get api/user
    
    * 'Negative test | Retrieve all users by Unauthorized user '
    
    * Update getUser.spec.ts
    
    change title
    
    ---------
    
    Co-authored-by: IQA-App <devopsteamiqa@gmail.com>

--- tests/api_tests/getUser.spec.ts
+++ tests/api_tests/getUser.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from '@playwright/test';
+import { API_URL_END_POINTS } from '../../apiData';
 
 test.skip('should check response status', { tag: ['@api'] }, async ({ request }) => {
     const response = await request.get(process.env.API_BASE_URL + '/user');
     console.log(JSON.stringify(response));
     expect(response['_initializer'].status).toBe(200);
 });
+test('Negative test|  Verify unauthorized user is unable to retrieve all users', async ({ request }) => {
+        const response = await request.get(process.env.API_BASE_URL + API_URL_END_POINTS.userEndPoint);
+        expect(response.status()).toBe(401);
+        
+        const responseMessage = await response.text();
+        expect(responseMessage).toContain('Unauthorized');
+    });
