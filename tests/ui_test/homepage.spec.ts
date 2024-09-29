d366091b5e63349890ff34ecb4975c894858bc4a
VViktoriia <vviktoriian@gmail.com>
2024-09-28 21:09:20 -0700

    Update Homepage (Lina) test

--- tests/ui_test/homepage.spec.ts
+++ tests/ui_test/homepage.spec.ts
@@ -6,8 +6,6 @@ test('Verify homepage URL', async ({ page }) => {
     await page.goto(process.env.UI_BASE_URL);
     await expect(page).toHaveURL('https://app.westus2.cloudapp.azure.com');
 
-    const homeText = page.locator('text=Home');
-    await expect(homeText).toBeVisible();
 });
 
     test('Verify homepage url and homepage content', async ({ page }) => {
