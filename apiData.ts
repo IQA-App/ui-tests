204204f08c664207ae32ad4b2f45e41148eeb9bc
VViktoriia <vviktoriian@gmail.com>
2024-11-13 10:57:06 -0800

    Add data to apiData and api transaction tests

--- apiData.ts
+++ apiData.ts
@@ -2,4 +2,9 @@ export const API_URL_END_POINTS = {
     userGetByIDEndPoint: '/auth/',
     userEndPoint: '/user/',
     categoryEndPoint: '/categories/',
+    createTransaction: '/transactions',
+    getTransaction: '/transactions/:id',
+    updateTransaction: '/transactions/:id',
+    deleteTransaction: '/transactions/:id',
+    transactionEndPoint: '/transactions'
 };
