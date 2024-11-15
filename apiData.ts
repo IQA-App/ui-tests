f4eff9f3b0dc739218460a921f61914d0e75ebdd
Elgrueber <elenagrueber@gmail.com>
2024-11-14 21:39:43 -0500

    add create transaction test

--- apiData.ts
+++ apiData.ts
@@ -2,9 +2,9 @@ export const API_URL_END_POINTS = {
     userGetByIDEndPoint: '/auth/',
     userEndPoint: '/user/',
     categoryEndPoint: '/categories/',
-    createTransaction: '/transactions',
+    createTransaction: '/transactions/',
     getTransaction: '/transactions/:id',
     updateTransaction: '/transactions/:id',
     deleteTransaction: '/transactions/:id',
-    transactionEndPoint: '/transactions'
+    transactionEndPoint: '/transactions',
 };
